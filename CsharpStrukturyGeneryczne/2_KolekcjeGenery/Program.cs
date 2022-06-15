971dc3f6fea74052463a3a74f12b08122a7b45eb
Przemek Kielpinski <przemyslaw.kielpinski@outlook.com>
2022-06-15 13:43:11 +0200

    wersja z obslugą tablic

--- CsharpStrukturyGeneryczne/2_KolekcjeGenery/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGenery/Program.cs
@@ -10,8 +10,28 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
+            Pracownik[] pracownik;
 
-          
+            pracownik = new Pracownik[]
+            {
+                new Pracownik {Imie = "Kata"},
+                new Pracownik {Imie = "Kat1", Nazwisko = "Nowa1k"},
+                new Pracownik {Imie = "Kata2", Nazwisko = "Nowak2"}
+            };
+
+            foreach (var p in pracownik)
+            {
+                Console.WriteLine(p.Imie + p.Nazwisko);
+                
+               
+            }
+
+            for (int i = 0; i < pracownik.Length; i++)
+            {
+                Console.WriteLine(pracownik[i].Imie + pracownik[i].Nazwisko);
+            }
+
+            Console.ReadLine();
         }
     }
 }
